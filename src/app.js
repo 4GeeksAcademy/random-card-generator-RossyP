359f596bc9f9c8f2cf58c6c9811423b265a6f2ed
Rossy Jasmin Palacin Nuñez <119419018+ROZZYNUEZ14@users.noreply.github.com>
2024-08-05 18:01:21 +0000

    Se realiza la creacion de la funcionalidad de las cartas

--- src/app.js
+++ src/app.js
@@ -5,7 +5,65 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+const headerSymbol = document.getElementById("symbol-header");
+const bodyNumber = document.getElementById("number-body");
+const footerSymbol = document.getElementById("symbol-footer");
+
+let symbols = ["hearts", "spades", "clubs", "diamonds"];
+let numbers = ["As", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+
+function random(arr) {
+  let randomNum = Math.floor(Math.random() * arr.length);
+  return randomNum;
+}
+
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const randomSymbol = symbols[random(symbols)];
+  const randomNumber = numbers[random(numbers)];
+
+  headerSymbol.style.fontSize = "100px";
+  footerSymbol.style.fontSize = "100px";
+
+  if (randomSymbol === "hearts") {
+    headerSymbol.innerHTML = "♥";
+    footerSymbol.innerHTML = "♥";
+    headerSymbol.style.color = "red";
+    footerSymbol.style.color = "red";
+    bodyNumber.style.color = "red";
+  } else if (randomSymbol === "spades") {
+    headerSymbol.innerHTML = "♠";
+    footerSymbol.innerHTML = "♠";
+  } else if (randomSymbol === "clubs") {
+    headerSymbol.innerHTML = "♣";
+    footerSymbol.innerHTML = "♣";
+  } else {
+    headerSymbol.innerHTML = "♦";
+    footerSymbol.innerHTML = "♦";
+    headerSymbol.style.color = "red";
+    footerSymbol.style.color = "red";
+    bodyNumber.style.color = "red";
+  }
+
+  if (randomNumber === "As") {
+    headerSymbol.innerHTML = "";
+    footerSymbol.innerHTML = "";
+    bodyNumber.style.fontSize = "220px";
+    if (randomSymbol === "hearts") {
+      bodyNumber.innerHTML = "♥";
+      bodyNumber.style.color = "red";
+    } else if (randomSymbol === "spades") {
+      bodyNumber.innerHTML = "♠";
+    } else if (randomSymbol === "clubs") {
+      bodyNumber.innerHTML = "♣";
+    } else {
+      bodyNumber.innerHTML = "♦";
+      bodyNumber.style.color = "red";
+    }
+  } else {
+    bodyNumber.innerHTML = randomNumber;
+    bodyNumber.style.fontSize = "180px";
+  }
+
+  console.log(randomSymbol);
+  console.log(randomNumber);
 };
