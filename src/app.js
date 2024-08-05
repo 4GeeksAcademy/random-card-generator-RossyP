cfc7fd81360937a1b4da4591e2caf9f785d7a2b1
Rossy Jasmin Palacin Nuñez <119419018+ROZZYNUEZ14@users.noreply.github.com>
2024-08-05 21:07:54 +0000

    Se agrega boton y se corrige el codigo de genereación de cartas

--- src/app.js
+++ src/app.js
@@ -8,8 +8,9 @@ import "./assets/img/4geeks.ico";
 const headerSymbol = document.getElementById("symbol-header");
 const bodyNumber = document.getElementById("number-body");
 const footerSymbol = document.getElementById("symbol-footer");
+const reload = document.getElementById("buttonReload");
 
-let symbols = ["hearts", "spades", "clubs", "diamonds"];
+let symbols = ["♥", "♠", "♣", "♦"];
 let numbers = ["As", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
 
 function random(arr) {
@@ -18,52 +19,42 @@ function random(arr) {
 }
 
 window.onload = function() {
+  generateCard();
+};
+
+function generateCard() {
   const randomSymbol = symbols[random(symbols)];
   const randomNumber = numbers[random(numbers)];
 
+  if (randomSymbol === "♥" || randomSymbol === "♦") {
+    headerSymbol.style.color = "red";
+    footerSymbol.style.color = "red";
+    bodyNumber.style.color = "red";
+  } else {
+    headerSymbol.style.color = "black";
+    footerSymbol.style.color = "black";
+    bodyNumber.style.color = "black";
+  }
+
   headerSymbol.style.fontSize = "100px";
   footerSymbol.style.fontSize = "100px";
-
-  if (randomSymbol === "hearts") {
-    headerSymbol.innerHTML = "♥";
-    footerSymbol.innerHTML = "♥";
-    headerSymbol.style.color = "red";
-    footerSymbol.style.color = "red";
-    bodyNumber.style.color = "red";
-  } else if (randomSymbol === "spades") {
-    headerSymbol.innerHTML = "♠";
-    footerSymbol.innerHTML = "♠";
-  } else if (randomSymbol === "clubs") {
-    headerSymbol.innerHTML = "♣";
-    footerSymbol.innerHTML = "♣";
-  } else {
-    headerSymbol.innerHTML = "♦";
-    footerSymbol.innerHTML = "♦";
-    headerSymbol.style.color = "red";
-    footerSymbol.style.color = "red";
-    bodyNumber.style.color = "red";
-  }
+  footerSymbol.style.transform = "rotate(180deg)";
 
   if (randomNumber === "As") {
     headerSymbol.innerHTML = "";
     footerSymbol.innerHTML = "";
+    bodyNumber.innerHTML = randomSymbol;
     bodyNumber.style.fontSize = "220px";
-    if (randomSymbol === "hearts") {
-      bodyNumber.innerHTML = "♥";
-      bodyNumber.style.color = "red";
-    } else if (randomSymbol === "spades") {
-      bodyNumber.innerHTML = "♠";
-    } else if (randomSymbol === "clubs") {
-      bodyNumber.innerHTML = "♣";
-    } else {
-      bodyNumber.innerHTML = "♦";
-      bodyNumber.style.color = "red";
-    }
   } else {
+    headerSymbol.innerHTML = randomSymbol;
     bodyNumber.innerHTML = randomNumber;
+    footerSymbol.innerHTML = randomSymbol;
     bodyNumber.style.fontSize = "180px";
   }
-
   console.log(randomSymbol);
   console.log(randomNumber);
-};
+}
+
+reload.addEventListener("click", function() {
+  generateCard();
+});
